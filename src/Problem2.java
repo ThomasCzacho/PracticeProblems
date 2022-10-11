e6b7a6d6dec91a41d30896dd0298af06dd1fd4a0
ThomasCzacho <Thomas.Czachorowski@cdpipelinedevshop.com>
2022-10-11 17:51:31 -0400

    Initial commit

---
+++ src/Problem2.java
@@ -0,0 +1,25 @@
+public class Problem2 {
+    public static void main(String[] args) {
+        //Use print statements to create your own beautiful original ASCII art.
+        //Use comments to describe what your image is depicting.
+
+        System.out.println("  XXXX ");
+        System.out.println(" XXXXXX ");
+        System.out.println(" XX||XX ");
+        System.out.println("   ||XX ");
+        System.out.println(" XX||/  ");
+        System.out.println("  \\||   ");
+        System.out.println("   ||   ");
+        System.out.println("  /||\\  ");
+// tree
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/Problem3.java
@@ -0,0 +1,14 @@
+public class Problem3 {
+
+    /*
+    Box Properties: Width, Depth, Height
+    Box Behaviors: Open-Close, True or False - Snake, Gift Wrap yes or no
+
+    Snake Properties: Color, Number of spots, Length
+    Snake Behaviors: Slither, Bite, Sleep
+
+    Customer Account Properties: Account Name, Box preset, Snake preset
+    Customer Account Behaviors: Change preset snake, change preset snake, Purchase
+     */
+
+}
